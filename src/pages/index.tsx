e94d97c257697c27c5ff21a624a80665e390715a
Edyta Badowska <edyta.badowska555@gmail.com>
2022-12-06 12:07:50 +0100

    feat: add function: getStaticProps.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,35 @@ import type { NextPageWithLayout } from "~/types/common.types";
 import Main from "~/views/Main/Main.view";
 import playlistData from "~/data/playlistsData.json";
 import { ModelWithId } from "~/models/Playlist.model";
+import dbConnect from "~/libraries/mongoose.library";
+import { getPlaylists } from "~/libraries/api.library";
+import { InferGetStaticPropsType } from "next";
 
-const Index: NextPageWithLayout = () => {
+export const getStaticProps = async () => {
+  await dbConnect();
+  const limit = 0;
+  const data = await getPlaylists(limit);
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      limit,
+      fallbackData: {
+        data,
+      },
+    },
+    revalidate: 60,
+  };
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Index: NextPageWithLayout<Props> = ({ fallbackData, limit }) => {
   return (
     <>
       <Head>
